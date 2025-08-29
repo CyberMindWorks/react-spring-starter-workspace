8e52c020e22b53a1a5e7877cf3e89b22184f20ca
aldrin <aldrin@cybermindworks.com>
2025-08-29 18:21:52 +0530

    Add react files

---
+++ react/vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    server: {
+        port: 3000,
+        host: "0.0.0.0",
+        allowedHosts: [".codeengineprod.blackbucks.me"],
+        proxy: {
+            '/api': {
+                target: "http://localhost:8000",
+                changeOrigin: true,
+            }
+        }
+    },
+    test: {
+        globals: true, // enables `describe`, `it` globally
+        environment: "jsdom", // simulate browser DOM
+        setupFiles: "./src/setupTests.js", // optional: setup file
+        reporters: ["json"],
+        outputFile: "result.json",
+    },
+});
