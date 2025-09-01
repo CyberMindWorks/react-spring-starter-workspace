2be5c6dc08e99845ae48e07bc9ebea4ec72280aa
aldrin <aldrin@cybermindworks.com>
2025-09-01 12:03:47 +0530

    rename result files

--- react/vite.config.js
+++ react/vite.config.js
@@ -3,23 +3,23 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 3000,
-        host: "0.0.0.0",
-        allowedHosts: [".codeengineprod.blackbucks.me"],
-        proxy: {
-            '/api': {
-                target: "http://localhost:8000",
-                changeOrigin: true,
-            }
-        }
+  plugins: [react()],
+  server: {
+    port: 3000,
+    host: "0.0.0.0",
+    allowedHosts: [".codeengineprod.blackbucks.me"],
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+      },
     },
-    test: {
-        globals: true, // enables `describe`, `it` globally
-        environment: "jsdom", // simulate browser DOM
-        setupFiles: "./src/setupTests.js", // optional: setup file
-        reporters: ["json"],
-        outputFile: "result.json",
-    },
+  },
+  test: {
+    globals: true, // enables `describe`, `it` globally
+    environment: "jsdom", // simulate browser DOM
+    setupFiles: "./src/setupTests.js", // optional: setup file
+    reporters: ["json"],
+    outputFile: "react-result.json",
+  },
 });
